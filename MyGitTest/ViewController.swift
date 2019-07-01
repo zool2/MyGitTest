bfaed1030786ae169f9759ac203eb2d8aa84e788
zool2 <jurije1993@gmail.com>
2019-07-01 16:56:18 +0900

    Initial Commit

---
+++ MyGitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyGitTest
+//
+//  Created by TJ on 01/07/2019.
+//  Copyright © 2019 zool2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
